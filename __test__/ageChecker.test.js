8037ec492f1c3be76bd5af8364b75c2b5e41aa0a
Gengur123 <meleearthur@gmail.com>
2021-06-25 10:15:15 -0700

    Added specs to read me

--- __test__/ageChecker.test.js
+++ __test__/ageChecker.test.js
@@ -9,7 +9,17 @@ describe("newAge",() =>{
 
   test('Should return the age',  () => {
     expect(age.age).toEqual(30);
-
   });
-
+  test('Should return the medium age',  () => {
+    expect(age.mediumAge).toEqual(90);
+  });
+  // test('Should return the age',  () => {
+  //   expect(age.age).toEqual(30);
+  // });
+  // test('Should return the age',  () => {
+  //   expect(age.age).toEqual(30);
+  // });
+  // test('Should return the age',  () => {
+  //   expect(age.age).toEqual(30);
+  // });
 });
